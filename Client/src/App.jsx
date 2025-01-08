521d16bb9aa6adcbd554de6840856cfe55b67084
Kamesh Chaudhary <kameshchaudhary17@gmail.com>
2025-01-08 08:28:30 +0545

    landing page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,15 +4,19 @@ import viteLogo from '/vite.svg'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Home from './pages/Home'
 import './App.css'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   return (
   <>
     <BrowserRouter>
      <Routes>
-      <Route path = '/' element = {<Login />}/>
+      <Route path = '/' element = {<LandingPage />}/>
+      <Route path = '/login' element = {<Login />}/>
       <Route path = '/signup' element = {<Signup />}/>
+      <Route path = '/home' element = {<Home />}/>
      </Routes>
      </BrowserRouter>
   </>
